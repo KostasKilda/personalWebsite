4c1de517cac687fb82c80e0a13ecda95cc00ebf7
KostasKilda <kostas.kilda@gmail.com>
2023-06-14 17:34:06 +0100

    Added projects page

---
+++ projects/app.js
@@ -0,0 +1,58 @@
+
+const hamburger = document.querySelector('.navbar-hamburger');
+const navbarAnchor = document.querySelectorAll('.navbar-anchor');
+const navbarListMobile = document.querySelector('.navbar-hidden-mobile');
+const navbarList = document.querySelector('.navbar-links');
+let mobileMenu = 0;
+
+hamburger.addEventListener('click', function () {
+    if (mobileMenu % 2 == 0) {
+        mobileMenu++;
+        for (let i = 0; i < navbarAnchor.length; i++) {
+            navbarAnchor[i].style.display = 'inline-block';
+        }
+        setTimeout(function () {
+            for (let i = 0; i < navbarAnchor.length; i++) {
+                setTimeout(function () {
+                    if (navbarAnchor[i].style.display != 'none') {
+                        navbarAnchor[i].style.transition = '0.5s';
+                        navbarAnchor[i].style.transform = 'translateY(0)';
+                    }
+                }, 50 + i * 50);
+            }
+        }, 200);
+    }
+    else {
+        for (let i = 0; i < navbarAnchor.length; i++) {
+            navbarAnchor[i].style.display = 'none';
+            navbarAnchor[i].style.transition = '0s';
+            navbarAnchor[i].style.transform = 'translateX(-100vw)';
+        }
+        mobileMenu++;
+    }
+    navbarListMobile.classList.toggle('navbar-links-toggle');
+});
+
+window.addEventListener("load", function () {
+    window.dispatchEvent(new Event('resize'));
+});
+
+window.onresize = function () {
+    if (window.innerWidth >= 896) {
+        if (navbarListMobile.classList[1] == 'navbar-links-toggle') {
+            navbarListMobile.classList.toggle('navbar-links-toggle');
+            mobileMenu = 0;
+        }
+        for (let i = 0; i < navbarAnchor.length; i++) {
+            navbarAnchor[i].style.display = 'inline-block';
+            navbarAnchor[i].style.transform = 'translateY(0)';
+        }
+    } else {
+        if (mobileMenu % 2 == 0) {
+            for (let i = 0; i < navbarAnchor.length; i++) {
+                navbarAnchor[i].style.display = 'none';
+                navbarAnchor[i].style.transform = 'translateX(-100vw)';
+            }
+        }
+    }
+}
